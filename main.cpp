768b780211a4eb65e05116ff7950aa5f7983f772
Yacine Ait Ali Yahia <111428692+YacIneTheBox@users.noreply.github.com>
2025-12-10 17:11:04 +0100

    soon adding audio

--- main.cpp
+++ main.cpp
@@ -4,9 +4,33 @@
 
 using namespace std;
 
-Color buttonColorCountour = WHITE;
-Color buttonColorFill = BLACK;
-Color textColor = WHITE;
+class Button {
+public:
+    Rectangle rectangle;
+    string text;
+    Color colorCountour;
+    Color colorFill;
+    Color textColor;
+
+    Button(Rectangle rect, string txt, Color countour, Color fill, Color text)
+        : rectangle(rect), text(txt), colorCountour(countour), colorFill(fill), textColor(text) {}
+
+    void WhenHoovered(){
+        if (CheckCollisionPointRec(GetMousePosition(),rectangle)){
+            colorFill = WHITE;
+            textColor = BLACK;
+        }else{
+            colorFill = BLACK;
+            textColor = WHITE;
+        }
+    }
+
+    void Draw(){
+        DrawRectangle(rectangle.x, rectangle.y, rectangle.width, rectangle.height, colorCountour);
+        DrawRectangle(rectangle.x+5, rectangle.y+5, rectangle.width-10, rectangle.height-10, colorFill);
+        DrawText(text.c_str(), rectangle.x+60, rectangle.y+20, 40, textColor);
+    }
+};
 
 
 void DrawingButtons(Rectangle cadre,string text);
@@ -14,9 +38,19 @@ int main() {
     const int SCREEN_WIDTH = 1200;
     const int SCREEN_HEIGHT = 800;
 
-    vector<Rectangle> buttons;
+    vector<Button> buttons;
     Rectangle MenuButton_Start = {SCREEN_WIDTH/(float)2 - 150, SCREEN_HEIGHT/(float)3 - 50, 300, 80};
-    buttons.push_back(MenuButton_Start);
+    Button MenuButton_Start_Button(MenuButton_Start, "Proceed", WHITE, BLACK, WHITE);
+    buttons.push_back(MenuButton_Start_Button);
+
+    Rectangle MenuButton_Settings = {SCREEN_WIDTH/(float)2 - 150, SCREEN_HEIGHT/(float)2 - 50, 300, 80};
+    Button MenuButton_Settings_Button(MenuButton_Settings,"Settings",WHITE,BLACK,WHITE);
+    buttons.push_back(MenuButton_Settings_Button);
+
+    Rectangle MenuButton_Exit = {SCREEN_WIDTH/(float)2 - 150, SCREEN_HEIGHT/(float)1.5f - 50, 300, 80};
+    Button MenuButton_Exit_Button(MenuButton_Exit,"Exit",WHITE,BLACK,WHITE);
+    buttons.push_back(MenuButton_Exit_Button);
+
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Menu Interactive");
     SetTargetFPS(60);
@@ -25,29 +59,19 @@ int main() {
 
         // hovering
         for (int i = 0;i<buttons.size();i++){
-            if (CheckCollisionPointRec(GetMousePosition(), buttons[i])){
-                buttonColorFill = WHITE;
-                textColor = BLACK;
-            }else{
-                buttonColorFill = BLACK;
-                textColor = WHITE;
-            }
+            buttons[i].WhenHoovered();
         }
 
         // drawing here
         BeginDrawing();
         ClearBackground(BLACK);
 
-        DrawingButtons(MenuButton_Start, "Proceed");
+        for (int i = 0;i<buttons.size();i++){
+            buttons[i].Draw();
+        }
 
         EndDrawing();
     }
 
     return 0;
-}
-
-void DrawingButtons(Rectangle cadre,string text){
-    DrawRectangle(cadre.x, cadre.y, cadre.width, cadre.height, buttonColorCountour);
-    DrawRectangle(cadre.x+5, cadre.y+5, cadre.width-10, cadre.height-10, buttonColorFill);
-    DrawText(text.c_str(), cadre.x+60, cadre.y+20, 40, textColor);
 }
